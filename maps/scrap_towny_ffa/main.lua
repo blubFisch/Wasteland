6c20eaea63ad773d5eaa97b8adcb183f9851827e
blubFisch <blubfisch@any.mx>
2022-10-24 16:40:55 +0100

    Change victory condition to tech score

--- maps/scrap_towny_ffa/main.lua
+++ maps/scrap_towny_ffa/main.lua
@@ -19,12 +19,12 @@ require 'maps.scrap_towny_ffa.trap'
 require 'maps.scrap_towny_ffa.turrets_drop_ammo'
 require 'maps.scrap_towny_ffa.vehicles'
 require 'maps.scrap_towny_ffa.suicide'
+require 'maps.scrap_towny_ffa.score'
 
 local Event = require 'utils.event'
 local Autostash = require 'modules.autostash'
 local MapDefaults = require 'maps.scrap_towny_ffa.map_defaults'
 local BottomFrame = require 'utils.gui.bottom_frame'
-local ScenarioTable = require 'maps.scrap_towny_ffa.table'
 local Nauvis = require 'maps.scrap_towny_ffa.nauvis'
 local Biters = require 'maps.scrap_towny_ffa.biters'
 local Pollution = require 'maps.scrap_towny_ffa.pollution'
@@ -33,130 +33,10 @@ local Team = require 'maps.scrap_towny_ffa.team'
 local Radar = require 'maps.scrap_towny_ffa.limited_radar'
 local Limbo = require 'maps.scrap_towny_ffa.limbo'
 local Evolution = require 'maps.scrap_towny_ffa.evolution'
-local mod_gui = require('mod-gui')
 local Gui = require 'utils.gui'
 local Color = require 'utils.color_presets'
 local Where = require 'utils.commands.where'
 local Inventory = require 'modules.show_inventory'
-
-local function spairs(t, order)
-    local keys = {}
-    for k in pairs(t) do
-        keys[#keys + 1] = k
-    end
-    if order then
-        table.sort(
-            keys,
-            function(a, b)
-                return order(t, a, b)
-            end
-        )
-    else
-        table.sort(keys)
-    end
-    local i = 0
-    return function()
-        i = i + 1
-        if keys[i] then
-            return keys[i], t[keys[i]]
-        end
-    end
-end
-
-local function init_score_board(player)
-    local this = ScenarioTable.get_table()
-    local saved_frame = this.score_gui_frame[player.index]
-    if saved_frame and saved_frame.valid then
-        return
-    end
-
-    local flow = mod_gui.get_frame_flow(player)
-    local frame = flow.add {type = 'frame', style = mod_gui.frame_style, caption = 'Town survival', direction = 'vertical'}
-    frame.style.vertically_stretchable = false
-    this.score_gui_frame[player.index] = frame
-end
-
-local function update_score()
-    local this = ScenarioTable.get_table()
-
-    for _, player in pairs(game.connected_players) do
-        local frame = this.score_gui_frame[player.index]
-        if not (frame and frame.valid) then
-            init_score_board(player)
-        end
-        if frame and frame.valid then
-            frame.clear()
-
-            local inner_frame = frame.add {type = 'frame', style = 'inside_shallow_frame', direction = 'vertical'}
-
-            local subheader = inner_frame.add {type = 'frame', style = 'subheader_frame'}
-            subheader.style.horizontally_stretchable = true
-            subheader.style.vertical_align = 'center'
-
-            subheader.add {type = 'label', style = 'subheader_label', caption = {'', 'Survive 2 days (48h) to win!'}}
-
-            if not next(subheader.children) then
-                subheader.destroy()
-            end
-
-            local information_table = inner_frame.add {type = 'table', column_count = 3, style = 'bordered_table'}
-            information_table.style.margin = 4
-            information_table.style.column_alignments[3] = 'right'
-
-            for _, caption in pairs({'Rank', 'Town (players online/total)', 'Survival time'}) do
-                local label = information_table.add {type = 'label', caption = caption}
-                label.style.font = 'default-bold'
-            end
-
-            local town_ages = {}
-            for _, town_center in pairs(this.town_centers) do
-                if town_center ~= nil then
-                    local age = game.tick - town_center.creation_tick
-                    town_ages[town_center] = age
-                end
-            end
-
-            local rank = 1
-
-            for town_center, age in spairs(
-                town_ages,
-                function(t, a, b)
-                    return t[b] < t[a]
-                end
-            ) do
-                local position = information_table.add {type = 'label', caption = '#' .. rank}
-                if town_center == this.town_centers[player.force.name] then
-                    position.style.font = 'default-semibold'
-                    position.style.font_color = {r = 1, g = 1}
-                end
-                local label =
-                    information_table.add {
-                    type = 'label',
-                    caption = town_center.town_name .. ' (' .. #town_center.market.force.connected_players .. '/' .. #town_center.market.force.players .. ')'
-                }
-                label.style.font = 'default-semibold'
-                label.style.font_color = town_center.color
-                local age_hours = age / 60 / 3600
-                information_table.add {type = 'label', caption = string.format('%.1f', age_hours) .. 'h'}
-
-                rank = rank + 1
-            end
-
-            -- Outlander section
-            information_table.add {type = 'label', caption = '-'}
-            local outlander_on = #game.forces['player'].connected_players + #game.forces['rogue'].connected_players
-            local outlander_total = #game.forces['player'].players + #game.forces['rogue'].players
-
-            local label =
-                information_table.add {
-                type = 'label',
-                caption = 'Outlanders' .. ' (' .. outlander_on .. '/' .. outlander_total .. ')'
-            }
-            label.style.font_color = {170, 170, 170}
-            information_table.add {type = 'label', caption = '-'}
-        end
-    end
-end
 
 local function on_init()
     Autostash.insert_into_furnace(true)
@@ -227,7 +107,6 @@ end
 Event.on_init(on_init)
 Event.on_nth_tick(60, on_nth_tick) -- once every second
 Event.on_nth_tick(60 * 30, ui_smell_evolution)
-Event.on_nth_tick(60, update_score)
 
 --Disable the comfy main gui since we good too many goodies there.
 Event.add(
---
+++ maps/scrap_towny_ffa/score.lua
@@ -0,0 +1,125 @@
+local Public = {}
+local mod_gui = require('mod-gui')
+
+local ScenarioTable = require 'maps.scrap_towny_ffa.table'
+local Event = require 'utils.event'
+
+local function spairs(t, order)
+    local keys = {}
+    for k in pairs(t) do
+        keys[#keys + 1] = k
+    end
+    if order then
+        table.sort(
+                keys,
+                function(a, b)
+                    return order(t, a, b)
+                end
+        )
+    else
+        table.sort(keys)
+    end
+    local i = 0
+    return function()
+        i = i + 1
+        if keys[i] then
+            return keys[i], t[keys[i]]
+        end
+    end
+end
+
+local function init_score_board(player)
+    local this = ScenarioTable.get_table()
+    local saved_frame = this.score_gui_frame[player.index]
+    if saved_frame and saved_frame.valid then
+        return
+    end
+
+    local flow = mod_gui.get_frame_flow(player)
+    local frame = flow.add {type = 'frame', style = mod_gui.frame_style, caption = 'Town survival', direction = 'vertical'}
+    frame.style.vertically_stretchable = false
+    this.score_gui_frame[player.index] = frame
+end
+
+local function update_score()
+    local this = ScenarioTable.get_table()
+    local score_to_win = 70
+
+    for _, player in pairs(game.connected_players) do
+        local frame = this.score_gui_frame[player.index]
+        if not (frame and frame.valid) then
+            init_score_board(player)
+        end
+        if frame and frame.valid then
+            frame.clear()
+
+            local inner_frame = frame.add {type = 'frame', style = 'inside_shallow_frame', direction = 'vertical'}
+
+            local subheader = inner_frame.add {type = 'frame', style = 'subheader_frame'}
+            subheader.style.horizontally_stretchable = true
+            subheader.style.vertical_align = 'center'
+
+            subheader.add {type = 'label', style = 'subheader_label', caption = {'', 'Reach ' .. score_to_win .. ' tech points to win!'}}
+
+            if not next(subheader.children) then
+                subheader.destroy()
+            end
+
+            local information_table = inner_frame.add {type = 'table', column_count = 3, style = 'bordered_table'}
+            information_table.style.margin = 4
+            information_table.style.column_alignments[3] = 'right'
+
+            for _, caption in pairs({'Rank', 'Town (players online/total)', 'Tech points'}) do
+                local label = information_table.add {type = 'label', caption = caption}
+                label.style.font = 'default-bold'
+            end
+
+            local town_tech_scores = {}
+            for _, town_center in pairs(this.town_centers) do
+                if town_center ~= nil then
+                    town_tech_scores[town_center] = town_center.evolution.worms * 100
+                end
+            end
+
+            local rank = 1
+
+            for town_center, tech_score in spairs(
+                    town_tech_scores,
+                    function(t, a, b)
+                        return t[b] < t[a]
+                    end
+            ) do
+                local position = information_table.add {type = 'label', caption = '#' .. rank}
+                if town_center == this.town_centers[player.force.name] then
+                    position.style.font = 'default-semibold'
+                    position.style.font_color = {r = 1, g = 1}
+                end
+                local label =
+                information_table.add {
+                    type = 'label',
+                    caption = town_center.town_name .. ' (' .. #town_center.market.force.connected_players .. '/' .. #town_center.market.force.players .. ')'
+                }
+                label.style.font = 'default-semibold'
+                label.style.font_color = town_center.color
+                information_table.add {type = 'label', caption = string.format('%.1f', tech_score)}
+
+                rank = rank + 1
+            end
+
+            -- Outlander section
+            information_table.add {type = 'label', caption = '-'}
+            local outlander_on = #game.forces['player'].connected_players + #game.forces['rogue'].connected_players
+            local outlander_total = #game.forces['player'].players + #game.forces['rogue'].players
+
+            local label =
+            information_table.add {
+                type = 'label',
+                caption = 'Outlanders' .. ' (' .. outlander_on .. '/' .. outlander_total .. ')'
+            }
+            label.style.font_color = {170, 170, 170}
+            information_table.add {type = 'label', caption = '-'}
+        end
+    end
+end
+
+Event.on_nth_tick(60, update_score)
