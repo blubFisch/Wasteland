35d52be8a73254ba639477d6375284dc150281a9
blubFisch <blubfisch@any.mx>
2023-04-18 17:59:50 +0100

    Customise and activate chatbot

--- utils/chatbot.lua
+++ utils/chatbot.lua
@@ -19,23 +19,13 @@ Global.register(
 local Public = {}
 
 local discord_url = "wasteland-discord.fun"
+local discord_url_styled = "[color=red]" .. discord_url .. "[/color]"
+
 
 local brain = {
-    [1] = {'Our Discord server is at: ' .. discord_url},
+    [1] = {"Our Discord server is at >> " .. discord_url_styled .. " >> Enter that URL into your web browser to join"},
     [2] = {
-        'Need an admin? Join our discord at: ' .. discord_url .. ',',
-        'and report it in #i-need-halp',
-        'If you have played for more than 5h in our maps then,',
-        'you are eligible to run the command /jail and /free'
-    },
-    [3] = {'Scenario repository for download:', 'https://github.com/ComfyFactory/ComfyFactorio'},
-    [4] = {
-        'If you feel like the server is lagging, run the following command:',
-        '/server-ups',
-        'This will display the server UPS on your top right screen.'
-    },
-    [5] = {
-        "If you're not trusted - ask an admin to trust you."
+        'Need an admin? Message in our discord at ' .. discord_url_styled
     }
 }
 
@@ -43,25 +33,15 @@ local links = {
     ['admin'] = brain[2],
     ['administrator'] = brain[2],
     ['discord'] = brain[1],
-    ['download'] = brain[3],
-    ['github'] = brain[3],
     ['greifer'] = brain[2],
     ['grief'] = brain[2],
     ['griefer'] = brain[2],
     ['griefing'] = brain[2],
     ['mod'] = brain[2],
     ['moderator'] = brain[2],
-    ['scenario'] = brain[3],
     ['stealing'] = brain[2],
     ['stole'] = brain[2],
     ['troll'] = brain[2],
-    ['stutter'] = brain[4],
-    ['freeze'] = brain[4],
-    ['lag'] = brain[4],
-    ['lagging'] = brain[4],
-    ['trust'] = brain[5],
-    ['trusted'] = brain[5],
-    ['untrusted'] = brain[5]
 }
 
 local function on_player_joined_game(event)
@@ -88,7 +68,7 @@ local function process_bot_answers(event)
     for word in string.gmatch(message, '%g+') do
         if links[word] then
             for _, bot_answer in pairs(links[word]) do
-                player.print('[font=heading-1]' .. bot_answer .. '[/font]', Color.warning)
+                player.print('Bot Reply: ' .. bot_answer, Color.warning)
             end
             return
         end
@@ -97,10 +77,6 @@ end
 
 local function on_console_chat(event)
     if not event.player_index then
-        return
-    end
-    local secs = Server.get_current_time()
-    if not secs then
         return
     end
     process_bot_answers(event)
