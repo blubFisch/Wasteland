52016260129618b492c22e2cccd117fb54c7c0ca
Stefan <sts17stefan@web.de>
2023-04-18 19:22:19 +0200

    changed or size and ore rate

--- maps/wasteland/rocks_yield_ore_veins.lua
+++ maps/wasteland/rocks_yield_ore_veins.lua
@@ -2,7 +2,6 @@ local math_random = math.random
 local math_floor = math.floor
 local table_insert = table.insert
 local table_shuffle = table.shuffle_table
-
 local ScenarioTable = require 'maps.wasteland.table'
 local valid_entities = {
     ['rock-big'] = true,
