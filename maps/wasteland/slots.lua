d1742692acf928c92f689c694643345a1bd4e8c1
blubFisch <blubfisch@any.mx>
2023-04-25 15:29:27 +0100

    Deduplicate and cleanup laser slots

--- maps/wasteland/slots.lua
+++ maps/wasteland/slots.lua
@@ -1,8 +1,6 @@
 local ScenarioTable = require 'maps.wasteland.table'
 
--- called whenever a player places an item
-local function on_built_entity(event)
-    local this = ScenarioTable.get_table()
+local function process_slots(actor, event)
     local entity = event.created_entity
     if not entity.valid then
         return
@@ -10,8 +8,8 @@ local function on_built_entity(event)
     if entity.name ~= 'laser-turret' then
         return
     end
-    local player = game.players[event.player_index]
-    local force = player.force
+    local this = ScenarioTable.get_table()
+    local force = actor.force
     local town_center = this.town_centers[force.name]
     local surface = entity.surface
     if force.index == game.forces['player'].index or force.index == game.forces['rogue'].index or town_center == nil then
@@ -23,7 +21,7 @@ local function on_built_entity(event)
                 color = {r = 0.77, g = 0.0, b = 0.0}
             }
         )
-        player.insert({name = 'laser-turret', count = 1})
+        actor.insert({name = 'laser-turret', count = 1})
         entity.destroy()
         return
     end
@@ -39,10 +37,19 @@ local function on_built_entity(event)
                 color = {r = 0.77, g = 0.0, b = 0.0}
             }
         )
-        player.insert({name = 'laser-turret', count = 1})
+
+        local inventory
+        if event.player_index then
+            inventory = actor
+        else
+            inventory = event.robot.get_inventory(defines.inventory.robot_cargo)
+        end
+        inventory.insert({name = entity.name, count = 1})
+
         entity.destroy()
         return
     end
+
     local key = script.register_on_entity_destroyed(entity)
     if (this.laser_turrets == nil) then
         this.laser_turrets = {}
@@ -50,6 +57,7 @@ local function on_built_entity(event)
     this.laser_turrets[key] = force.index
     locations = locations + 1
     town_center.upgrades.laser_turret.locations = locations
+
     surface.create_entity(
         {
             name = 'flying-text',
@@ -60,66 +68,14 @@ local function on_built_entity(event)
     )
 end
 
--- called whenever a robot places an item
+local function on_player_built_entity(event)
+    process_slots(game.get_player(event.player_index), event)
+end
+
 local function on_robot_built_entity(event)
-    local this = ScenarioTable.get_table()
-    local entity = event.created_entity
-    if not entity.valid then
-        return
-    end
-    if entity.name ~= 'laser-turret' then
-        return
-    end
-    local robot = event.robot
-    local force = robot.force
-    local town_center = this.town_centers[force.name]
-    local surface = entity.surface
-    if force.index == game.forces['player'].index or force.index == game.forces['rogue'].index or town_center == nil then
-        surface.create_entity(
-            {
-                name = 'flying-text',
-                position = entity.position,
-                text = 'Robot not acclimated to this technology!',
-                color = {r = 0.77, g = 0.0, b = 0.0}
-            }
-        )
-        robot.insert({name = 'laser-turret', count = 1})
-        entity.destroy()
-        return
-    end
-    local slots = town_center.upgrades.laser_turret.slots
-    local locations = town_center.upgrades.laser_turret.locations
-    if locations >= slots then
-        surface.create_entity(
-            {
-                name = 'flying-text',
-                position = entity.position,
-                text = 'Town does not have enough slots!',
-                color = {r = 0.77, g = 0.0, b = 0.0}
-            }
-        )
-        robot.insert({name = 'laser-turret', count = 1})
-        entity.destroy()
-        return
-    end
-    local key = script.register_on_entity_destroyed(entity)
-    if (this.laser_turrets == nil) then
-        this.laser_turrets = {}
-    end
-    this.laser_turrets[key] = force.index
-    locations = locations + 1
-    town_center.upgrades.laser_turret.locations = locations
-    surface.create_entity(
-        {
-            name = 'flying-text',
-            position = entity.position,
-            text = 'Using ' .. locations .. '/' .. slots .. ' slots',
-            color = {r = 1.0, g = 1.0, b = 1.0}
-        }
-    )
+    process_slots(event.robot, event)
 end
 
--- called whenever a laser-turret is removed from the map
 local function on_entity_destroyed(event)
     local key = event.registration_number
     local this = ScenarioTable.get_table()
@@ -147,6 +103,6 @@ local function on_entity_destroyed(event)
 end
 
 local Event = require 'utils.event'
-Event.add(defines.events.on_built_entity, on_built_entity)
+Event.add(defines.events.on_built_entity, on_player_built_entity)
 Event.add(defines.events.on_robot_built_entity, on_robot_built_entity)
 Event.add(defines.events.on_entity_destroyed, on_entity_destroyed)
