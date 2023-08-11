764393035960dc3e730dfec4fb75e2381604397d
blubFisch <blubfisch@any.mx>
2023-08-11 16:13:52 +0100

    Add alliance chat

--- modules/global_chat_toggle.lua
+++ modules/global_chat_toggle.lua
@@ -1,41 +1,50 @@
-local function toggle(player)
-    if not player.gui.top.global_chat_toggle then
-        return
-    end
+global.chat_modes = {}
+
+local CHAT_MODES = {GLOBAL = 1, TEAM = 2, ALLIANCE = 3}
+local BUTTON_PROPERTIES = {
+    [CHAT_MODES.GLOBAL] = {'Global Chat', 'Chat messages are sent to everyone.', {r = 0.0, g = 0.77, b = 0.0}},
+    [CHAT_MODES.TEAM] = {'Team Chat', 'Chat messages are only sent to your team.', {r = 0.77, g = 0.77, b = 0.0}},
+    [CHAT_MODES.ALLIANCE] = {'Alliance Chat', 'Chat messages are only sent to your alliance.', {r = 0.5, g = 0.6, b = 0.9}}
+}
+
+local function setChatMode(player, mode)
+    global.chat_modes[player.index] = mode
 
     local button = player.gui.top.global_chat_toggle
+    local properties = BUTTON_PROPERTIES[mode]
+    button.caption = properties[1]
+    button.tooltip = properties[2]
+    button.style.font_color = properties[3]
+end
 
-    if button.caption == 'Global Chat' then
-        button.caption = 'Team Chat'
-        button.tooltip = 'Chat messages are only sent to your team.'
-        button.style.font_color = {r = 0.77, g = 0.77, b = 0.0}
-        return
+local function toggle(player)
+    local button = player.gui.top.global_chat_toggle
+    if not button then return end
+
+    local current_mode = global.chat_modes[player.index] or CHAT_MODES.GLOBAL
+
+    if current_mode == CHAT_MODES.GLOBAL then
+        setChatMode(player, CHAT_MODES.TEAM)
+    elseif current_mode == CHAT_MODES.TEAM then
+        setChatMode(player, CHAT_MODES.ALLIANCE)
+    else
+        setChatMode(player, CHAT_MODES.GLOBAL)
     end
-
-    button.caption = 'Global Chat'
-    button.tooltip = 'Chat messages are sent to everyone.'
-    button.style.font_color = {r = 0.0, g = 0.77, b = 0.0}
 end
 
 local function create_gui_button(player)
-    if player.gui.top.global_chat_toggle then
-        return
-    end
-    local b =
-        player.gui.top.add(
-        {
-            type = 'sprite-button',
-            name = 'global_chat_toggle',
-            caption = ''
-        }
-    )
-    b.style.font = 'heading-2'
-    b.style.minimal_width = 100
-    b.style.minimal_height = 38
-    b.style.maximal_height = 38
-    b.style.padding = 1
-    b.style.margin = 0
-    toggle(player)
+    local button = player.gui.top.global_chat_toggle
+    if button then return end
+
+    button = player.gui.top.add({type = 'sprite-button', name = 'global_chat_toggle', caption = ''})
+    button.style.font = 'heading-2'
+    button.style.minimal_width = 100
+    button.style.minimal_height = 38
+    button.style.maximal_height = 38
+    button.style.padding = 1
+    button.style.margin = 0
+
+    setChatMode(player, CHAT_MODES.GLOBAL)
 end
 
 local function on_player_joined_game(event)
@@ -44,40 +53,44 @@ local function on_player_joined_game(event)
 end
 
 local function on_gui_click(event)
-    if not event then
-        return
-    end
-    if not event.element then
-        return
-    end
-    if not event.element.valid then
-        return
-    end
+    if not event or not event.element or not event.element.valid then return end
+
     local player = game.players[event.element.player_index]
-    if event.element.name ~= 'global_chat_toggle' then
-        return
+    if event.element.name == 'global_chat_toggle' then
+        toggle(player)
     end
-    toggle(player)
 end
 
 local function on_console_chat(event)
-    if not event.message then
-        return
-    end
-    if not event.player_index then
-        return
-    end
-    local player = game.players[event.player_index]
-    if not player.gui.top.global_chat_toggle then
-        return
-    end
-    local button = player.gui.top.global_chat_toggle
-    if button.caption ~= 'Global Chat' then
-        return
-    end
-    for _, force in pairs(game.forces) do
-        if force.name ~= player.force.name then
-            force.print(player.name .. ' ' .. player.tag .. ': ' .. event.message, player.chat_color)
+    if not event.message or not event.player_index then return end
+
+    local sender = game.players[event.player_index]
+    local button = sender.gui.top.global_chat_toggle
+    if not button then return end
+
+    local message = sender.name .. ' ' .. sender.tag .. ': ' .. event.message
+    local currentMode = global.chat_modes[sender.index]
+
+    -- Note:
+    -- The engine already displays messages to
+    -- 1. the sending player
+    -- 2. the team of the sending player
+
+    if currentMode == CHAT_MODES.GLOBAL then
+        for _, force in pairs(game.forces) do
+            if force ~= sender.force then
+                for _, player in pairs(force.players) do
+                    player.print(message, sender.chat_color)
+                end
+            end
+        end
+    elseif currentMode == CHAT_MODES.ALLIANCE then
+        for _, force in pairs(game.forces) do
+            if force.get_friend(sender.force) and force ~= sender.force then
+                for _, player in pairs(force.players) do
+                    player.print(message, sender.chat_color)
+                end
+            end
         end
     end
 end
