0363c34a08711ea9a866a6ff61e0ab101c366bd0
ZwerOxotnik <zweroxotnik@gmail.com>
2024-03-31 12:43:28 +0300

    Improve

--- modules/train_protection.lua
+++ modules/train_protection.lua
@@ -9,6 +9,19 @@ local _allow_ally_connection = false
 local _allow_connection_with_neutral = false
 
 
+local _render_text_position = {0, 0}
+local draw_text = rendering.draw_text
+local _render_target_forces = {nil}
+local _render_text_param = {
+	text = {"train_protection.warning"},
+	target = _render_text_position,
+	surface = nil,
+	forces = _render_target_forces,
+	scale = 1,
+	time_to_live = 210,
+	color = {255, 0, 125}
+}
+
 --#region Functions of events
 
 
@@ -17,16 +30,54 @@ local _allow_connection_with_neutral = false
 ---@param first_carriage LuaEntity
 local function disconnect_train(train, entity, first_carriage)
 	train.speed = 0
-	entity.disconnect_rolling_stock(defines.rail_direction.front)
-	if first_carriage.valid then
-		first_carriage.train.speed = 0.1
 
-		local passengers = first_carriage.train.passengers
-		for i = 1, #passengers do
-			local passenger = passengers[i]
-			if passenger.valid then
-				passenger.vehicle.set_driver(nil)
-			end
+	local neutral_force = game.forces.neutral
+	local force = entity.force
+	local back_entity = entity.get_connected_rolling_stock(defines.rail_direction.back)
+	if back_entity and back_entity.valid then
+		local back_force = back_entity.force
+		if force ~= back_force and
+				not (_allow_connection_with_neutral and back_force == neutral_force) and
+				not (_allow_ally_connection and (force.get_cease_fire(back_force) and
+				back_force.get_cease_fire(force) and
+				force.get_friend(back_force) and
+				back_force.get_friend(force)))
+		then
+			entity.disconnect_rolling_stock(defines.rail_direction.back)
+		end
+	end
+
+	local front_entity = entity.get_connected_rolling_stock(defines.rail_direction.front)
+	if front_entity and front_entity.valid then
+		local front_force = front_entity.force
+		if force ~= front_force and
+				not (_allow_connection_with_neutral and front_force == neutral_force) and
+				not (_allow_ally_connection and (force.get_cease_fire(front_force) and
+				front_force.get_cease_fire(force) and
+				force.get_friend(front_force) and
+				front_force.get_friend(force)))
+		then
+			entity.disconnect_rolling_stock(defines.rail_direction.front)
+		end
+	end
+
+	if not first_carriage.valid then return end
+	local passengers = first_carriage.train.passengers
+	if passengers == 0 then return end
+
+	first_carriage.train.speed = 0.1
+
+	for i = 1, #passengers do
+		local passenger = passengers[i]
+		if passenger.valid then
+			passenger.vehicle.set_driver(nil)
+
+			_render_text_position[1] = passenger.position.x
+			_render_text_position[2] = passenger.position.y
+			-- Show warning text
+			_render_target_forces[1] = passenger.force
+			_render_text_param.surface = passenger.surface
+			draw_text(_render_text_param)
 		end
 	end
 end
@@ -40,9 +91,10 @@ function M.on_train_created(event)
 	if not train.valid then return end
 
 	local neutral_force = game.forces.neutral
-	local first_carriage = train.carriages[1]
-	local force = first_carriage.force
 	local carriages = train.carriages
+	local first_carriage = carriages[1]
+
+	local force = first_carriage.force
 	for i = 2, #carriages do
 		local carriage = carriages[i]
 		local _force = carriage.force
@@ -54,7 +106,7 @@ function M.on_train_created(event)
 			_force.get_friend(force)))
 		then
 			disconnect_train(train, carriage, first_carriage)
-			break
+			return
 		end
 	end
 end
