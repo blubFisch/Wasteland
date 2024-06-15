c68e264e3af960f7e342c1ec22d170364f6b8d2f
blubFisch <blubfisch@any.mx>
2024-06-15 09:24:40 +0100

    Increase starter wall size slightly - prevents shooting the town center from outside

--- maps/wasteland/info.lua
+++ maps/wasteland/info.lua
@@ -2,14 +2,12 @@ local Public = {}
 
 local changelog =
     [[[font=heading-2]Jun 2024 updates[/font]
+ - Increase starter wall size slightly - prevents shooting the town center from outside
  - In survival mode, league 4 towns only get survival time score when offline (only when offline for 2h or longer + including allied towns)
  - Laser turret distance adjustments + Before researching laser turret, can build max 8
  - Fluid tanks are no longer explosive - this caused too much frustration for beginners
  - Tank shells are now very effective against other tanks with direct hits
  - Time score now only counts survival time without shield (=online or L4)
- - Less cost increase for adding players to a town
- - Battle mode (="Short) has no yellow/purple science + smaller towns
- - Exploit fixes
  - When a town is offline, it gains a rest bonus, which is applied to player damage output and research cost]]
 
 local info =
