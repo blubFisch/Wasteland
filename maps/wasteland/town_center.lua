a8d1fe493c608ceb836cfc13bdeb2f142d4297c2
blubFisch <blubfisch@any.mx>
2025-02-23 17:36:06 +0800

    Retain explored map when founding a town

--- maps/wasteland/town_center.lua
+++ maps/wasteland/town_center.lua
@@ -523,6 +523,7 @@ local function found_town(event)
     --log("setting spawn point = {" .. spawn_point.x .. "," .. spawn_point.y .. "}")
     force.set_spawn_position(pos, surface)
 
+    force.copy_chart(player.force, surface, surface)
     Team.add_player_to_town(player, town_center)
     Team.update_chart_tag(town_center, force)
 
